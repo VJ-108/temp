9c642408c5d29122a8d2f4b020e065b8b26c33e3
VJ-108 <vaibhav160205@gmail.com>
2025-11-22 09:16:27 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost.nip.io:3000/api";
-export const SERVER_URL = "http://localhost.nip.io:3000";
+export const API_BASE_URL = "http://56.228.23.204.nip.io:3000/api";
+export const SERVER_URL = "http://56.228.23.204.nip.io:3000";
